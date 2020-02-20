cc72c82e01c55ce080f19312ec7ef6a42cece7f4
CadenaR <a01652141@itesm.mx>
2020-02-20 15:35:23 +0100

    Authorization provider for User and Shelter

---
+++ animalshelter/src/main/java/es/sidelab/animalshelter/UserShelterAuthProvider.java
@@ -0,0 +1,77 @@
+package es.sidelab.animalshelter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserShelterAuthProvider implements AuthenticationProvider {
+
+		@Autowired
+		private UserRepository userRepository;
+		
+		@Autowired
+		private ShelterRepository shelterRepository;
+
+		@Autowired
+		private UserShelterComponent userShelterComponent;
+
+		/*
+		 * This method checks for a user or a shelter by email entered in login form, if none is found
+		 * it throws an error, but if one of them is found it checks which type is and gives authorization
+		 */
+		@Override
+		public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+			String email = authentication.getName();
+			String password = (String) authentication.getCredentials();
+	
+			User user = userRepository.findByUserEmail(email);
+			Shelter shelter = shelterRepository.findByShelterEmail(email);
+
+			if (user == null && shelter == null) {
+				throw new BadCredentialsException("User not found");
+			}
+			
+			if(user != null) {
+				
+				if (!new BCryptPasswordEncoder().matches(password, user.getUserPassword())) {
+					throw new BadCredentialsException("Wrong password");
+				} else {
+
+					userShelterComponent.setLoggedUser(user);
+					List<GrantedAuthority> roles = new ArrayList<>();
+					roles.add(new SimpleGrantedAuthority("ROLE_USER"));
+					return new UsernamePasswordAuthenticationToken(email, password, roles);
+				}
+			} else {
+				 
+				if (!new BCryptPasswordEncoder().matches(password, shelter.getShelterPassword())) {
+					throw new BadCredentialsException("Wrong password");
+				} else {
+
+					userShelterComponent.setLoggedUser(shelter);
+					List<GrantedAuthority> roles = new ArrayList<>();
+					roles.add(new SimpleGrantedAuthority("ROLE_SHELTER"));
+					return new UsernamePasswordAuthenticationToken(email, password, roles);
+				}
+			}
+
+			
+		}
+
+		@Override
+		public boolean supports(Class<?> authenticationObject) {
+			return true;
+		}
+	}
