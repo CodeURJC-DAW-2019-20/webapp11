5958f0ce545cdd355fb3909bccc8477df67eec1f
CadenaR <a01652141@itesm.mx>
2020-02-20 15:34:39 +0100

    Public and roled URLs and authentication config

--- animalshelter/src/main/java/es/sidelab/animalshelter/SecurityConfiguration.java
+++ animalshelter/src/main/java/es/sidelab/animalshelter/SecurityConfiguration.java
@@ -1,57 +1,67 @@
 package es.sidelab.animalshelter;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration 
 @EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 	
-	@Autowired
-	DataSource dataSource;
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
-	}
+    String[] resources = new String[]{
+            "/fonts/**","/css/**","/img/**","/js/**","/scss/**"
+    };
+    
+    @Autowired
+	public UserShelterAuthProvider userShelterAuthProvider;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
 
-	// Enable jdbc authentication
-	@Autowired
-	public void configAuthentication(AuthenticationManagerBuilder auth) throws Exception {
-		auth.jdbcAuthentication().dataSource(dataSource).passwordEncoder(passwordEncoder());
-	}
-
-	@Bean
-	public JdbcUserDetailsManager jdbcUserDetailsManager() throws Exception {
-		JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager();
-		jdbcUserDetailsManager.setDataSource(dataSource);
-		return jdbcUserDetailsManager;
+		// Database authentication provider
+		auth.authenticationProvider(userShelterAuthProvider);
 	}
 	
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 	
-	// Public pages
-	//http.authorizeRequests().antMatchers("/").permitAll();
-
+		// Public pages
+		http.authorizeRequests().antMatchers(resources).permitAll();
+		http.authorizeRequests().antMatchers("/").permitAll();
+		http.authorizeRequests().antMatchers("/login").permitAll();
+		http.authorizeRequests().antMatchers("/animals").permitAll();
+		http.authorizeRequests().antMatchers("/contact").permitAll();
+		http.authorizeRequests().antMatchers("/animalview").permitAll();
+		http.authorizeRequests().antMatchers("/signuser").permitAll();
+		http.authorizeRequests().antMatchers("/signshelter").permitAll();
+		http.authorizeRequests().antMatchers("/createShelter").permitAll();
+		http.authorizeRequests().antMatchers("/createUser").permitAll();
 		
-	// Disable CSRF at the moment
-	http.csrf().disable();
+		// URLs that need authentication to access to it
+		http.authorizeRequests().antMatchers("/profile").hasRole("USER");
+		http.authorizeRequests().antMatchers("/animalform").hasRole("SHELTER");
+		http.authorizeRequests().antMatchers("/createAnimal").hasRole("SHELTER");
+		http.authorizeRequests().antMatchers("/request").hasRole("SHELTER");
+		
+		// Login form
+	    http.formLogin().loginPage("/");
+		http.formLogin().loginProcessingUrl("/login");
+	    http.formLogin().usernameParameter("username");
+	    http.formLogin().passwordParameter("password");
+	    http.formLogin().defaultSuccessUrl("/");
+	    http.formLogin().failureUrl("/");
+	    
+	 // Logout
+        http.logout().logoutUrl("/logout");
+        http.logout().logoutSuccessUrl("/");
+	
+			
+		// Disable CSRF at the moment
+		http.csrf().disable();
 	}
  
 }
