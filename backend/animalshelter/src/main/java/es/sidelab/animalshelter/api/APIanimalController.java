08ac821722e6f2d837d039555929095ec8900f07
ArshiaSaleem98 <arshiasaleem98@gmail.com>
2020-03-06 00:35:06 +0100

    api rest url created (get and post animal image)

--- backend/animalshelter/src/main/java/es/sidelab/animalshelter/api/APIanimalController.java
+++ backend/animalshelter/src/main/java/es/sidelab/animalshelter/api/APIanimalController.java
@@ -1,8 +1,11 @@
 package es.sidelab.animalshelter.api;
 
+import java.io.IOException;
 import java.util.Collection;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,15 +15,22 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
 import es.sidelab.animalshelter.Animal;
+import es.sidelab.animalshelter.controllers.ImageService;
 
 @RestController
 @RequestMapping("/api/animals")
 public class APIanimalController {
 
 	private Map<Long, Animal> animals = new ConcurrentHashMap<>();
+	@Autowired
+	private ImageService imageService;
+
 
 	@GetMapping("/")
 	public Collection<Animal> animals() {
@@ -69,6 +79,28 @@ public class APIanimalController {
 
 		if (animal != null) {
 			return new ResponseEntity<>(animal, HttpStatus.OK);
+		} else {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+	}
+	@PostMapping("/{id}/image")//to post animalPhoto by postman
+	@ResponseStatus(HttpStatus.CREATED)
+	public Animal setAnimalImage(@RequestParam(value="files", required=false) MultipartFile file, @PathVariable long id)  throws IOException {
+		
+		Animal animal = animals.get(id);
+		imageService.saveImage("animals", animal.getIdAnimal(), file);
+		animal.setAnimalPhoto("image-" + animal.getIdAnimal() + ".jpg");
+		
+		return animal;
+	}
+	
+	@GetMapping("/{id}/image")
+	public ResponseEntity<String> getAnimalimage(@PathVariable long id) {
+
+		Animal animal = animals.get(id);
+		
+		if (animal != null) {
+			return new ResponseEntity<>(animal.getAnimalPhoto(), HttpStatus.OK);
 		} else {
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
