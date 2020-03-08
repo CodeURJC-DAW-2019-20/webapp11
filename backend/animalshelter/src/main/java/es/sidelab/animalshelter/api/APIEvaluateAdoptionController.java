601a02dd2654245a93952a64198f81a6f2d923e5
CadenaR <a01652141@itesm.mx>
2020-03-08 21:15:53 +0000

    Adoption evaluation rest controller

---
+++ backend/animalshelter/src/main/java/es/sidelab/animalshelter/api/APIEvaluateAdoptionController.java
@@ -0,0 +1,57 @@
+package es.sidelab.animalshelter.api;
+
+import javax.mail.MessagingException;
+import javax.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import es.sidelab.animalshelter.Adoption;
+import es.sidelab.animalshelter.AdoptionRepository;
+import es.sidelab.animalshelter.Animal;
+import es.sidelab.animalshelter.AnimalRepository;
+import es.sidelab.animalshelter.SmtpMailSender;
+
+@Controller
+@RequestMapping("/api/evaluateAdoption")
+public class APIEvaluateAdoptionController {
+
+	@Autowired
+	SmtpMailSender smtpMailSender;
+
+	@Autowired
+	private AnimalRepository ar;
+
+	@Autowired
+	private AdoptionRepository ap;
+
+	@RequestMapping("/accept")
+	public Adoption accept(String animalName, HttpServletRequest request) throws MessagingException {
+
+		Animal a = ar.findByAnimalName(animalName);
+		Adoption adoption = ap.findByAnimal(a);
+
+		adoption.confirmAdoption();
+		ap.save(adoption);
+
+		smtpMailSender.sendAccept("Adoption accepted", adoption.getUser().getUserEmail());
+
+		return adoption;
+	}
+
+	@RequestMapping("/deny")
+	public Adoption deny(String animalName, HttpServletRequest request) throws MessagingException {
+
+		Animal a = ar.findByAnimalName(animalName);
+		a.setAnimalAdopted(false);
+		Adoption adoption = ap.findByAnimal(a);
+
+		adoption.confirmAdoption();
+		ar.save(a);
+		ap.save(adoption);
+
+		smtpMailSender.sendDeny("Adoption rejected", adoption.getUser().getUserEmail());
+
+		return adoption;
+	}
+
+}
